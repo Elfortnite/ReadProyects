c89ded09f9a8f06bacb48c07527419e05b5ef6ce
David <davidrr.dev@gmail.com>
2023-12-13 14:24:59 +0100

    Avance 3 en raya

--- 01-3-raya/src/App.jsx
+++ 01-3-raya/src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/sol.png'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
+function Profile() {
+  return (
+    <img src={reactLogo}
+      
+      alt="Katherine Johnson"
+    />
+  )
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <section>
+				<input type="text" name="NOMBRE" placeholder="Search for a city..."></input>
+
+      <h1>Stockholm, SE</h1>
+      <Profile />
+      <h2>Clear Sky</h2>
+      <h3>thursday 14:20 PM</h3>
+      <p>Wind 5.7 m/s</p>
+      <p>Pressure 1015 hPa</p>
+      <p>Humidity 50%</p>
+      <p> Cloudiness 0%</p>
+
+    </section>
+  );
 }
 
 export default App
