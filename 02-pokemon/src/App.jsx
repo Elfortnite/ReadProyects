d24e85253a23906037f8eedc2dd7a734edd29824
David <davidrr.dev@gmail.com>
2024-01-30 14:27:43 +0100

    pke

--- 02-pokemon/src/App.jsx
+++ 02-pokemon/src/App.jsx
@@ -1,23 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-import {imagenurl} from './imgpoke'
 
 // "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151" -> 1 fetch
 // "https://pokeapi.co/api/v2/pokemon/11/" -> 2 fetch (por cada pokemon [1-151])
 //https://pokeapi.co/api/v2/pokemon/2/
 
 function Avatar() {
-  const [count, setCount] = useState(0)
+  const [pokemones, setpokemones] = useState([])
+   useEffect(()=>{
+    const getpokemones = async() =>{
 
+      const reponse= await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+      const listapokemones =await reponse.json()
+      const {results} = listapokemones
+
+      const pokemones= results.map(async(pokemon)=>{
+        const reponse = await fetch (pokemon.url)
+        const poke = await reponse.json()
+
+
+        return{
+          id: poke.id,
+          name:poke.name,
+          img: poke.sprites.other.dream_world.front_dafault
+        }
+      })
+      setpokemones(await Promise.all(newopokemones))
+    }
+   
+    getpokemones()
+   },[])
   return (
     <>
       <div>
-        <img
-        className='rata'
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png"
-        />
+        <h1>hholas</h1>
+        {
+          pokemones.map(pokemon =>{
+            return(
+              <div>
+                <img src={pokemon.img} alt={pokemon.name}/>
+                <p>{pokemon.name}</p>
+                <span>{pokemon.id}</span>
+              </div>
+          )
+          })
+        }
       </div>
       
     </>
--- 02-pokemon/src/imgpoke.jsx
+++ 02-pokemon/src/imgpoke.jsx
@@ -1,8 +1,43 @@
-export function imagenurl(pokemon,size='s'){
-    return(
-        'https://pokeapi.co/api/v2/pokemon/19/'+
-        pokemon.imageId +
-        size +
-        '.png'
-    );
+import { useEffect, useState } from 'react'
+import './App.css'
+
+// "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151" -> 1 fetch
+// "https://pokeapi.co/api/v2/pokemon/11/" -> 2 fetch (por cada pokemon [1-151])
+//https://pokeapi.co/api/v2/pokemon/2/
+
+function Avatar() {
+  const [pokemones, setpokemones] = useState([])
+   useEffect(()=>{
+    const getpokemones = async() =>{
+
+      const reponse= await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+      const listapokemones =await reponse.json()
+      const {results} = listapokemones
+
+      setpokemones(results)
+
+    }
+    getpokemones()
+   })
+  return (
+    <>
+      <div>
+        <h1>hholas</h1>
+        {
+          pokemones.map(pokemon => <p>{pokemon.name}</p>)
+        }
+      </div>
+      
+    </>
+  )
 }
+
+export default function App () {
+  return(
+    <div>
+      <Avatar/>
+    </div>
+    )
+}
+
+
